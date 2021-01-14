cd7c867b9b144868d136e052b5e5bcc81e31805e
eb110 <wfigura@op.pl>
2021-01-14 18:42:37 +0000

    inicjalizacja

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace PortalRandkowy.API.Controllers
+{
+    // http://localhost:5000/api/Values
+    [Route("api/[controller]")]
+    [ApiController] 
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "lola"};
+        }
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return id.ToString();
+        }
+       
+    }
+
+
+}

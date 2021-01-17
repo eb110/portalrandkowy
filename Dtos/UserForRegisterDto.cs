d1cc70bc612c15631294766e1e097146c5125b96
eb110 <wfigura@op.pl>
2021-01-17 18:36:27 +0000

    walidacja podczas rejestracji

--- Dtos/UserForRegisterDto.cs
+++ Dtos/UserForRegisterDto.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace PortalRandkowy.API.Dtos
 {
     public class UserForRegisterDto
     {
+        [Required(ErrorMessage="Nazwa wymagana")]
         public string Username { get; set; }
+        [Required(ErrorMessage="Has£o wymagane")]
+        [StringLength(12, MinimumLength=8, ErrorMessage="Has£o musi miec od 8 do 12 znaków")]
         public string Password { get; set; }
     }
 }
